36423d96bb1ad3cb627d891038729ae20a44c3ca
SU - NB0165 <52479864+irnshafa@users.noreply.github.com>
2021-10-21 16:03:57 +0700

    register page

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
