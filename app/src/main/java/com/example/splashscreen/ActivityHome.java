b9c183de8df3ce479a518dd53e46651a8cd0b0bd
SU - NB0165 <52479864+irnshafa@users.noreply.github.com>
2021-10-22 10:26:27 +0700

    loginpage

--- app/src/main/java/com/example/splashscreen/ActivityHome.java
+++ app/src/main/java/com/example/splashscreen/ActivityHome.java
@@ -2,7 +2,9 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityHome extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ActivityHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void MoveLayout(View view) {
+        Intent intent = new Intent(ActivityHome.this, ActivityLogin.class);
+        startActivity(intent);
     }
 }
