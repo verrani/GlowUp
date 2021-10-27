69e05a9d1fd36235b11a9848d87b88304745a387
verrani <verraniandini@gmail.com>
2021-10-27 22:55:44 +0700

    homepage

--- app/src/main/java/com/example/splashscreen/ActivityLogin.java
+++ app/src/main/java/com/example/splashscreen/ActivityLogin.java
@@ -2,7 +2,9 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityLogin extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void MoveLayout(View view) {
+        Intent intent = new Intent(ActivityLogin.this, homepage.class);
+        startActivity(intent);
     }
 }
