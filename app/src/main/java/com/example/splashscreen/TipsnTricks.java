a3cd6c5f09f80e8b3271be943f5d13cff13745cc
agugnjr <aginanjar151@gmail.com>
2021-10-28 22:44:25 +0700

    Tips and Trick
    TipsnTrick page done at10.44 pm

--- app/src/main/java/com/example/splashscreen/TipsnTricks.java
+++ app/src/main/java/com/example/splashscreen/TipsnTricks.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class homepage extends AppCompatActivity {
+public class TipsnTricks extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_tipsn_tricks);
     }
     public void MoveLayout(View view) {
-        Intent intent = new Intent(homepage.this, TipsnTricks.class);
+        Intent intent = new Intent(TipsnTricks.this, Article.class);
         startActivity(intent);
     }
 }
