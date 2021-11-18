e1f321b5f955940b8154138081d6130cce732d56
meitamerdianti123 <82669604+meitamerdianti123@users.noreply.github.com>
2021-11-18 11:52:11 +0700

    Quit update. Tp masih blm bisa sih. quitnya balik ke home :')

--- app/src/main/java/com/example/splashscreen/ProfileAccount.java
+++ app/src/main/java/com/example/splashscreen/ProfileAccount.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import androidx.appcompat.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
 public class ProfileAccount extends AppCompatActivity {
 
@@ -13,10 +16,27 @@ public class ProfileAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button button_alert = findViewById(R.id.button);
+        button_alert.setOnClickListener(view -> showDialog());
     }
 
-    public void homepage(View view) {
-        Intent intent = new Intent(ProfileAccount.this, homepage.class);
-        startActivity(intent);
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        alertDialogBuilder.setTitle("Quit from app?");
+
+        alertDialogBuilder
+                .setMessage("Press Yes to quit")
+                .setIcon(R.mipmap.ic_launcher)
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> ProfileAccount.this.finish())
+                .setNegativeButton("Cancel", (dialog, id) -> dialog.cancel());
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
+
+
 }
