6dc82ae553d83a1fb9790b824da372e94c6a01b9
verrani <verraniandini@gmail.com>
2021-11-29 16:16:49 +0700

    carousel

--- app/src/main/java/com/example/splashscreen/homepage.java
+++ app/src/main/java/com/example/splashscreen/homepage.java
@@ -5,13 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class homepage extends AppCompatActivity {
+    private int[] mImages = new int[]{
+            R.drawable.one, R.drawable.two, R.drawable.three
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        CarouselView carouselView = findViewById(R.id.carousel);
+        carouselView.setPageCount(mImages.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(mImages[position]);
+            }
+        });
+
     }
     public void MoveLayout(View view) {
         Intent intent = new Intent(homepage.this, TipsnTricks.class);
